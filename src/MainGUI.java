895d2131e56be52d615b7eca7308caa0aae36d3f
andyhernandez-rangel970-collab <andy.hernandez-rangel.970@my.csun.edu>
2025-10-22 22:22:47 -0700

    controller test

---
+++ src/MainGUI.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+
+
+
+public class MainGUI extends Application{
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Jaca Hotel");
+        Label label = new Label("Welcome to Jaca Hotel");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
