3d56bbcd5db72817fda7889ac1bef556a1de589c
angel <angel.cruz-marcial.613@my.csun.edu>
2025-11-21 02:01:44 -0800

    cart java doc ready

--- src/cart.java
+++ src/cart.java
@@ -1,30 +1,55 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
-//keeps an observable list so TableViews update automatically
-// allows you to add, remove, and clear rooms
-//checks if the cart is empty and calculates the total price
-
-
+/**
+ * Class: cart
+ * Date: 11/20/2025
+ * @author Angel Cruz
+ * @version 1.0
+ *
+ * Maintains a collection of selected hotel rooms as cartItems
+ * provides a list of items that can automatically updates the Tableview when changes happen in the cart
+ * The cart makes sure the same room isn’t added more than once
+ * The cart lets you add, remove, clear rooms, check if the cart is empty, and view the total price
+ *
+ */
 public class cart {
-    private ObservableList<cartItem> items = FXCollections.observableArrayList(); // updates the UI when rooms are changed
-
+    /**
+     * Holds all the items in the cart and automatically updates the display whenever something changes
+    */
+    private ObservableList<cartItem> items = FXCollections.observableArrayList();
+    /**
+     * Gets the observable list of cart items
+     */
     public ObservableList<cartItem> getItems() { return items; }
-
-    public void add(room r, hotel h) // adds a room from a certain hotel to the cart, also prevents duplicates from room ID
+    /**
+     * Adds a room from a hotel if the same room id is not already in the cart, preventing duplicate items
+     * @param r the room to add
+     * @param h the hotel that owns the room
+     * returns true if a new item was added
+     * returns false if inputs were null or a duplicate room id
+     */
+    public void add(room r, hotel h)
     {
         if (r == null || h == null) return;
         boolean exists = items.stream().anyMatch(ci -> ci.selectedRoom.getID() == r.getID());
         if (!exists) items.add(new cartItem(r, h));
     }
-
-    public void remove(cartItem ci) {items.remove(ci);}  //removes one row
-
+    /**
+     * Removes a certain cart item in the cart
+     * @parem ci the cartitem to remove
+     */
+    public void remove(cartItem ci) {items.remove(ci);}
+    /**
+     * Clears all items in the cart
+     */
     public void clear() { items.clear(); }
-
-    public boolean isEmpty() { return items.isEmpty();} // empty check
-
-
-    public double total() { return items.stream().mapToDouble(cartItem::price).sum(); } //sum of the total
+    /**
+     * Checks if the cart currently is empty
+     */
+    public boolean isEmpty() { return items.isEmpty();}
+    /**
+     * Calculates the total price of all items currently in the cart
+     */
+    public double total() { return items.stream().mapToDouble(cartItem::price).sum(); }
 }
 
