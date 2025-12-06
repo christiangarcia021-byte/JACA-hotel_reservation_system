3b4c521084cdb88aa1cd0392eddbfe9b0e0de980
andyhernandez-rangel970-collab <andy.hernandez-rangel.970@my.csun.edu>
2025-12-05 16:24:35 -0800

    Updated the database. You may need to manually delete some old tables (order_log). Remaned txt to v1.3

---
+++ testing/dbUtilTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+public class dbUtilTest {
+
+    @org.junit.Test
+    public void getHotelName() {
+    }
+
+    @org.junit.Test
+    public void getRoomName() {
+    }
+
+    @org.junit.Test
+    public void getCurrentTime() {
+    }
+
+    @org.junit.Test
+    public void getCurrentDate() {
+    }
+}
